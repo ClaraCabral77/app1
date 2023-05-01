acba87110c8dc465936c25cdcf4f862547244ad7
ladyboy <cabralclara77@gmail.com>
2023-05-01 11:24:04 -0300

    list

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Input} from './input/index';
+export { default as Events } from '../events/index';
+export { default as CustomModal } from '../modal/index'
+
---
+++ src/events/item/styles.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        height: 60,
+        justifyContent: 'center',
+        backgroundColor: '#F3D8C7',
+        borderRadius: 40,
+        marginVertical: 5,
+    },
+    item: {
+        marginHorizontal: 10,
+        color: 'white',
+        fontSize: 16,
+        fontWeight: 'bold',
+    },
+});
